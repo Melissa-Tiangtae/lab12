bf2b68ae72cf6202f6f0f3cf669f3ab2b33d1d3a
Melissa Tiangtae <melissa_tiangate@cmu.ac.th>
2024-12-24 16:00:51 +0700

    lab12_1_fixed_completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -6,11 +6,9 @@ long long int fibonacci(int);
 
 int main(){
 
-    clock_t start = clock();
-    cout << "Result: " << fibonacci(50) << "\n";
-    clock_t end = clock();
-    double elapsed = double(end - start)/CLOCKS_PER_SEC;
-    cout << "Elapsed Time: " << elapsed << " seconds.";
+     int x;
+    cin >> x;
+    cout << fibonacci(x);
     return 0;
 
 
